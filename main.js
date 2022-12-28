5315cfd10fa9e4a104676c7dd3e3508418b69f5b
Naviphoenix <i.salinas.marquez@gmail.com>
2022-12-28 01:01:21 -0600

    Diplomados done and fix feedback

--- main.js
+++ main.js
@@ -1,52 +1,27 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-var __webpack_exports__ = {};
-
-;// CONCATENATED MODULE: ./src/components/home/courses-carousel/carousel.js
-class CoursesCarousel {
-  constructor () {
-    this.init()
-  }
-
-  init() {
-    let coursesCarousel = document.querySelector('#coursesCarousel')
-    let flkt = new Flickity(coursesCarousel, {
-      cellAlign: 'left',
-      contain: true,
-      pageDots: false,
-      prevNextButtons: false,
-    })
-  }
-}
-
-/* harmony default export */ const carousel = (new CoursesCarousel);
-;// CONCATENATED MODULE: ./src/components/home/modals/register.js
-
-let showPasswordBtn = document.querySelector('#showPassword')
-let passwordField = document.querySelector('#passwordField')
-
-class RegisterModal {
-  constructor() {
-
-    showPasswordBtn.addEventListener('click', this.showPassword)
-  }
-
-  showPassword ( ) {
-    if (passwordField.getAttribute('type') == 'password') {
-      passwordField.setAttribute('type', 'text')
-    } else {
-      passwordField.setAttribute('type', 'password')
+(() => {
+    "use strict";
+    new class {
+        constructor() {
+            this.init()
+        }
+        init() {
+            let t = document.querySelector("#coursesCarousel");
+            new Flickity(t, {
+                cellAlign: "left",
+                contain: !0,
+                pageDots: !1,
+                prevNextButtons: !1
+            })
+        }
+    };
+    let t = document.querySelector("#showPassword"),
+        e = document.querySelector("#passwordField");
+    new class {
+        constructor() {
+            t.addEventListener("click", this.showPassword)
+        }
+        showPassword() {
+            "password" == e.getAttribute("type") ? e.setAttribute("type", "text") : e.setAttribute("type", "password")
+        }
     }
-  }
-}
-
-/* harmony default export */ const register = (new RegisterModal());
-;// CONCATENATED MODULE: ./src/index.js
-
-
-// Home
-
-
-
-/******/ })()
-;
+  })();
