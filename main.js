c2572c0dc3999d203af802e12efcc082d5791731
Naviphoenix <i.salinas.marquez@gmail.com>
2023-01-02 21:07:36 -0600

    Remove fontawesome

--- main.js
+++ main.js
@@ -1,27 +1,116 @@
-(() => {
-    "use strict";
-    new class {
-        constructor() {
-            this.init()
-        }
-        init() {
-            let t = document.querySelector("#coursesCarousel");
-            new Flickity(t, {
-                cellAlign: "left",
-                contain: !0,
-                pageDots: !1,
-                prevNextButtons: !1
-            })
-        }
-    };
-    let t = document.querySelector("#showPassword"),
-        e = document.querySelector("#passwordField");
-    new class {
-        constructor() {
-            t.addEventListener("click", this.showPassword)
-        }
-        showPassword() {
-            "password" == e.getAttribute("type") ? e.setAttribute("type", "text") : e.setAttribute("type", "password")
-        }
-    }
-  })();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/styles.scss":
+/*!*************************!*\
+  !*** ./src/styles.scss ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://webpack/./src/styles.scss?");
+
+/***/ }),
+
+/***/ "./src/components/home/courses-carousel/carousel.js":
+/*!**********************************************************!*\
+  !*** ./src/components/home/courses-carousel/carousel.js ***!
+  \**********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nclass CoursesCarousel {\n  constructor () {\n    this.init()\n  }\n\n  init() {\n    let coursesCarousel = document.querySelector('#coursesCarousel')\n    let flkt = new Flickity(coursesCarousel, {\n      cellAlign: 'left',\n      contain: true,\n      pageDots: false,\n      prevNextButtons: false,\n    })\n  }\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (new CoursesCarousel);\n\n//# sourceURL=webpack://webpack/./src/components/home/courses-carousel/carousel.js?");
+
+/***/ }),
+
+/***/ "./src/components/home/modals/register.js":
+/*!************************************************!*\
+  !*** ./src/components/home/modals/register.js ***!
+  \************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n\nlet showPasswordBtn = document.querySelector('#showPassword')\nlet passwordField = document.querySelector('#passwordField')\n\nclass RegisterModal {\n  constructor() {\n\n    showPasswordBtn.addEventListener('click', this.showPassword)\n  }\n\n  showPassword ( ) {\n    if (passwordField.getAttribute('type') == 'password') {\n      passwordField.setAttribute('type', 'text')\n    } else {\n      passwordField.setAttribute('type', 'password')\n    }\n  }\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (new RegisterModal());\n\n//# sourceURL=webpack://webpack/./src/components/home/modals/register.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles.scss */ \"./src/styles.scss\");\n/* harmony import */ var _components_home_courses_carousel_carousel__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/home/courses-carousel/carousel */ \"./src/components/home/courses-carousel/carousel.js\");\n/* harmony import */ var _components_home_modals_register__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/home/modals/register */ \"./src/components/home/modals/register.js\");\n\n\n// Home\n\n\n\n\n//# sourceURL=webpack://webpack/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
